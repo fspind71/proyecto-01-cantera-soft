56ef300b33b64296ba0e584201573c40ed87924a
Fabián Espíndola <fspind@gmail.com>
2022-07-25 16:50:49 -0300

    Mi primer commit

---
+++ algoritmos.js
@@ -0,0 +1,65 @@
+let a = 5
+let b = 9
+
+function(a,b){
+    return (a + b);
+}
+
+
+var miNombre = "Fabián";
+var miApellido = "Espíndola";
+
+function nombre(){
+    return (miNombre + miApellido);
+
+}
+
+
+var edad = 51
+
+if(edad == 51) {
+    console.log("puedes viajar");
+} else {
+    console. log("aún no puedes viajar");
+}
+
+var numero = 7;
+
+switch (numero) {
+    case 7:
+        console.log("Soy siete");
+        break;
+    case 70;
+        console.log ("Soy setenta");
+        break;
+    case 700;
+        console.log ("Soy setecientos")
+        break;
+    default:
+        console.log ("No soy nada")
+
+}
+
+let n1 = 5;
+let n2 = 4;
+
+const sumar = n1 + n2;
+
+if(resultado > 9) {
+    return console.log ("el valor es mayor que 9")
+} else {
+    return console.log ("el valor es menor que 9")
+}
+
+function resultado (){
+    let n1 = 5;
+    let n2 = 4;
+
+    const suma =n1 + n2;
+
+    if (resultado > 9) {
+        return console.log ("el valor es mayor que 9")
+    } else {
+        return console.log ("el valor es menor que 9")
+    }
+}
